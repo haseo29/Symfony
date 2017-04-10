2d8441930fc504a09050bca00303639b72f451db
haseo29 <anthonyledeunff@orange.fr>
2017-04-10 16:04:04 +0200

    commit du lundi aprem

--- src/ALD/PremierTestBundle/Controller/AdvertController.php
+++ src/ALD/PremierTestBundle/Controller/AdvertController.php
@@ -9,7 +9,99 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
-class AdvertController extends Controller {
+class AdvertController extends Controller
+{
+  public function indexAction($page)
+  {
+    if ($page < 1) {
+      throw new NotFoundHttpException('Page "'.$page.'" inexistante.');
+    }
+    // Notre liste d'annonce en dur
+    $listAdverts = array(
+      array(
+        'title'   => 'Recherche développpeur Symfony',
+        'id'      => 1,
+        'author'  => 'Alexandre',
+        'content' => 'Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…',
+        'date'    => new \Datetime()),
+      array(
+        'title'   => 'Mission de webmaster',
+        'id'      => 2,
+        'author'  => 'Hugo',
+        'content' => 'Nous recherchons un webmaster capable de maintenir notre site internet. Blabla…',
+        'date'    => new \Datetime()),
+      array(
+        'title'   => 'Offre de stage webdesigner',
+        'id'      => 3,
+        'author'  => 'Mathieu',
+        'content' => 'Nous proposons un poste pour webdesigner. Blabla…',
+        'date'    => new \Datetime())
+    );
+    return $this->render('testBundle:Advert:index.html.twig', array(
+      'listAdverts' => $listAdverts,
+    ));
+  }
+  public function viewAction($id)
+  {
+    $advert = array(
+      'title'   => 'Recherche développpeur Symfony',
+      'id'      => $id,
+      'author'  => 'Alexandre',
+      'content' => 'Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…',
+      'date'    => new \Datetime()
+    );
+    return $this->render('testBundle:Advert:view.html.twig', array(
+      'advert' => $advert
+    ));
+  }
+  public function addAction(Request $request)
+  {
+    // Si la requête est en POST, c'est que le visiteur a soumis le formulaire
+    if ($request->isMethod('POST')) {
+      $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
+      // Puis on redirige vers la page de visualisation de cettte annonce
+      return $this->redirectToRoute('oc_platform_view', array('id' => 5));
+    }
+    // Si on n'est pas en POST, alors on affiche le formulaire
+    return $this->render('testBundle:Advert:add.html.twig');
+  }
+  public function editAction($id, Request $request)
+  {
+    if ($request->isMethod('POST')) {
+      $request->getSession()->getFlashBag()->add('notice', 'Annonce bien modifiée.');
+      return $this->redirectToRoute('oc_platform_view', array('id' => 5));
+    }
+    $advert = array(
+      'title'   => 'Recherche développpeur Symfony',
+      'id'      => $id,
+      'author'  => 'Alexandre',
+      'content' => 'Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…',
+      'date'    => new \Datetime()
+    );
+    return $this->render('testBundle:Advert:edit.html.twig', array(
+      'advert' => $advert
+    ));
+  }
+  public function deleteAction($id)
+  {
+    return $this->render('testBundle:Advert:delete.html.twig');
+  }
+  public function menuAction($limit)
+  {
+    // On fixe en dur une liste ici, bien entendu par la suite on la récupérera depuis la BDD !
+    $listAdverts = array(
+      array('id' => 2, 'title' => 'Recherche développeur Symfony'),
+      array('id' => 5, 'title' => 'Mission de webmaster'),
+      array('id' => 9, 'title' => 'Offre de stage webdesigner')
+    );
+    return $this->render('testBundle:Advert:menu.html.twig', array(
+      // Tout l'intérêt est ici : le contrôleur passe les variables nécessaires au template !
+      'listAdverts' => $listAdverts
+    ));
+  }
+}
+
+/*class AdvertController extends Controller {
     /* public function indexAction($page)
       {
       if($page<1)
@@ -32,7 +124,7 @@ class AdvertController extends Controller {
       array('var1' =>$var1,'var2' =>$var2));
 
       mail('moi@openclassrooms.com', 'inscription OK', $contenu);
-      } */
+      } 
 
     public function indexAction($page) {
 
@@ -88,7 +180,7 @@ class AdvertController extends Controller {
         }
         
       
-    }*/
+    }
 
      public function addAction(Request $request) {//si la requete est en POST, c'est que le visiteur a soumis le formulaire
       if ($request->isMethod('POST')) {
@@ -210,7 +302,7 @@ class AdvertController extends Controller {
 
 
       return $this->redirectToRoute('testBundle', array ('id' =>5));
-      } */
+      } 
 
 
     public function viewSlugAction($slug, $year, $_format) {
@@ -226,4 +318,4 @@ class AdvertController extends Controller {
         return new Response($content);
     }
 
-}
+}*/
